b712e8971e94f8607d8eb1651cbb2a0af045eb5b
liangqi <136625007@qq.com>
2016-11-15 18:14:20 +0800

    update t.js

--- t.js
+++ t.js
@@ -4,8 +4,8 @@ const Inspecte = require('./index.js');
 const Promise = require("bluebird");
 
 let province = '陕西省 ';
-let city     = ' 宝鸡市';
-let area     = '金台区 ';
+let city     = ' 西安市';
+let area     = '雁塔区 ';
 
 // 检查地址
 Inspecte.check(province, city, area)
